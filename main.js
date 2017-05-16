882be70d1e760b9f504418852211db841e326e1e
sweeper3000 <sweeper3000@users.noreply.github.com>
2017-05-16 14:19:52 -0400

    Refactor special selecting js

---
+++ main.js
@@ -0,0 +1,12 @@
+function rotateSpecials() {
+		var pizzas = ["Large Pepperoni Pizza", "One Medium", "Vegetarian Pizza", "Meat Lover's Pizza", "Two Medium Pizzas and 2 Drinks", "Party Size with 4 Drinks", "Small Pizza"];
+		var specialText = "Today's special is ";
+		var imgs = ["imgs/large-pepperoni.png", "imgs/medium-pizza.jpg", "imgs/vegetarian.jpg", "imgs/meat-lovers.jpg", "imgs/2-medium-with-2-drinks.jpg", "imgs/party-size-with-4-drinks.png", "imgs/small.jpg"];
+		
+		var date = new Date();
+		var currentDate = date.getDay();
+		
+		$("#special-img").attr("src", imgs[currentDate]);
+		document.getElementById("special-img-txt").innerHTML = pizzas[currentDate];
+		document.getElementById("special").innerHTML = specialText + pizzas[currentDate];	
+}
